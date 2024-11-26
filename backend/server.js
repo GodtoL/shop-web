efa660d9199ebe3c097c7563864817bc535c2277
Elias <eliasvera793@gmail.com>
2024-11-26 10:20:37 -0300

    Se crea las rutas de de post y get

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,33 @@
 const express = require('express')
 const app = express()
-const Product = require('./models/product')
+app.use(express.json())
+const Product = require('./models/Product')
+
 require('./config/db')
 
 app.get("/", async (req, res) => {
-    res.send("¡Bienvenidos gamers!")
-    d
+    //res.send("¡Bienvenidos gamers!")
+    try{
+        const products = await Product.find();
+        res.status(200).send(products)
+    } catch (error){
+        console.error("Error al obtener productos", error)
+
+    }
+})
+
+app.post("/add", async (req, res) => {
+    try{
+        const {title, price, imageUrl} = req.body;
+        const newProduct = new Product({title, price, imageUrl})
+        await newProduct.save();
+        res.status(201).send("Cargado exitosamente");
+
+    } catch (error){
+        console.error("Error al obtener productos", error);
+        res.status(500).send("Hubo un problema al intentar guardar");
+
+    }
 })
 
 port = 3000
