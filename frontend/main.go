eab7738da0875d67ac4f64c13e64368df6dfaf1b
Elias <eliasvera793@gmail.com>
2024-11-27 21:52:02 -0300

    Se crea la ruta principal

--- frontend/main.go
+++ frontend/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"context"
 	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 var mongoClient *mongo.Client
@@ -25,6 +27,7 @@ func main() {
 			"message": "hello world",
 		})
 	})
+	r.GET("/home", getProducts)
 	r.Run()
 }
 
@@ -45,4 +48,20 @@ func connect_to_mongodb() error {
 
 	mongoClient = client
 	return nil
+}
+
+func getProducts(c *gin.Context) {
+	cursor, err := mongoClient.Database("gamer-shop").Collection("products").Find(context.TODO(), bson.D{{}})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"Error" : err.Error()})
+		return
+	}
+
+	var products []bson.M
+	if err = cursor.All(context.TODO(), &products); err != nil{
+		c.JSON(http.StatusInternalServerError, gin.H{"Error" : err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, products)
 }
