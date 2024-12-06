570317b79c36891e2f3ec2dc04f0dbee06c2ad81
Elias <eliasvera793@gmail.com>
2024-12-06 15:44:40 -0300

    boton de entregado

--- backend/controllers/manageController.js
+++ backend/controllers/manageController.js
@@ -19,4 +19,20 @@ const getOrders = async(req, res) => {
         console.error("Error al obtener productos", error)
         res.status(404).json({message : "Hubo un error al intentar obtener productos"})
 }}
-module.exports = { manage }
+
+const deleteOrder = async(req, res) => {
+    const {id} = req.params
+    try{
+        console.log("el id ", id)
+        const order = await Order.findOneAndDelete(id)
+        console.log("el pedido ", order)
+        if(!order){
+            console.log("Error")
+        }
+        //wait Order.findByIdAndDelete(order.id)
+
+    } catch(error) {
+        console.log("error")
+    }
+}
+module.exports = { manage, deleteOrder }
--- backend/routes/manageRoute.js
+++ backend/routes/manageRoute.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { manage } = require('../controllers/manageController');
+const { manage, deleteOrder} = require('../controllers/manageController');
 const authenticate = require('../middlewares/Authenticate')
 
+
 router.get("/" , manage)
+router.post("/delete/:id", deleteOrder)
 
 module.exports = router;
